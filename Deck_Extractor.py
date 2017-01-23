5b1c954b7d246969f7493d44bae4f024d33278d2
ABZB <abzb13@gmail.com>
2017-01-23 17:03:28 -0500

    First Upload

---
+++ Deck_Extractor.py
@@ -0,0 +1,124 @@
+import sqlite3
+import sys
+import os
+import string
+import shutil
+
+def extract(deck_name):
+
+	#path for pulling .cdb files
+	pathways = []
+	with open("paths.txt") as f:
+		pathways = f.readlines()
+		f.close()
+		
+	cdb_path = pathways[1]
+	cdb_path = cdb_path.strip()
+	output_path = pathways[3]
+	
+	deck_path = cdb_path + "deck/" #pathway to deck folder
+	output_path = output_path.strip()
+
+	deck_name = deck_path + deck_name + '.ydk'
+        
+	#array to store found card names while iterating through databases
+	temp_array = []
+	
+	#check in order to create array in first run, then overwrite as needed in subsequent iterations.
+	data_number = 0
+	
+	#open .ydk file
+	with open(deck_name) as f:
+		#read card ids from .ydk file
+		deck_list_temp = f.readlines()
+		f.close()
+		
+		
+	copied_cdb_path = os.getcwd() + "/"
+	
+	
+	#iterarates through every .cdb file in directory. Will fail if program is in different folder than the .cdb files.
+	for file in [doc for doc in os.listdir(copied_cdb_path) if doc.endswith(".cdb")]:
+		con = sqlite3.connect(file)
+		with con:
+			c = con.cursor()
+			#iterate through all the lines.
+			for i in range(len(deck_list_temp)):
+				#put id number in a format to be parsed by sql
+				t = (str(deck_list_temp[i]),)
+				#grabs every name that matches card id (should only be 1)
+				c.execute("SELECT name FROM texts WHERE id=?", t)
+				cardname = c.fetchone()
+				#if the current .cdb does not have current card id...
+				if cardname is None:
+					#if this is the first .cdb, append the card id number as a placeholder
+					if data_number == 0:
+						temp_array.append(str(deck_list_temp[i]))
+				#if card id is found
+				else:
+					cardname = str(cardname)
+					cardname = cardname.replace("',)","")
+					cardname = cardname.replace("('","")
+					cardname = cardname.replace('!side',"Side Deck: ")
+					cardname = cardname.replace('#extra',"Extra Deck: ")
+					cardname = cardname.replace('#main',"Main Deck: ")
+					#if this is the first .cdb, append card name
+					if data_number == 0:
+						temp_array.append(cardname)
+					#otherwise overwrite card id with card name in the array
+					else:
+						temp_array[i] = cardname
+		data_number += 1
+						
+	
+	
+	#request name for output file
+	end_name = input('Enter name for output file: ')
+	end_name += '.txt'
+	end_name = output_path + end_name
+	with open(end_name, 'w') as d:
+		for i in temp_array:
+			if i == "Main Deck: " or i == "Extra Deck: " or i == "Side Deck: ":
+				d.write("\n")
+				
+			d.write(i)
+			d.write("\n")
+
+def	update_cdbs():
+	#grab paths from text file
+	pathways = []
+	with open("paths.txt") as f:
+		pathways = f.readlines()
+		f.close()
+	cdb_path = pathways[1]
+	main_folder_path = pathways[1]
+
+	#remove newline character
+	main_folder_path = main_folder_path.strip()
+	cdb_path = cdb_path.strip() + "expansions/" #pathway to .cdb folder
+
+	#If they add new subfolders, update this list
+	directories_list = ['',"live//","live2016//","live2017//","liveanime//"]
+
+	#append various subfolders
+	for iter in range(len(directories_list)):
+		directories_list[iter] = cdb_path + directories_list[iter]
+
+	#get current directory
+	current_directory = os.getcwd() + '//'
+
+	#avoids overwriting of same filename in different subfolders
+	filenumber = 0
+
+	#copy over .cdb databases
+	for paths in directories_list:
+		for file in [doc for doc in os.listdir(paths) if doc.endswith(".cdb")]:
+			shutil.copy2(paths + os.path.basename(file), current_directory + str(filenumber) + os.path.basename(file))
+			filenumber += 1 
+	shutil.copy2(main_folder_path + 'cards.cdb', current_directory + 'cards.cdb')
+
+			
+
+deck_name = input('Input Deck Name: ')
+update_cdbs()
+extract(deck_name)
---
+++ readme.txt
@@ -0,0 +1,21 @@
+Distributed Attribution-NonCommercial-ShareAlike 4.0 International License
+by Aryeh (Andrew) Baruch (AKA ABZB13 AKA The Count of Real Numbers)
+
+V 2.0
+
+Requirements:
+Python 3.X (sqlite 3 might have to be downloaded seperately)
+
+Installation instructions:
+1) Place paths.txt, Set_up.py, and Deck_Extractor.py in some folder convenient to you. I myself place them in a new folder: /Ygopro/Deck_Extractor/
+2) Open paths.txt
+	a) Change the second line to the pathway to your YGOpro folder (ending in /ygopro/)
+	b) Change the fourth line to the path whichever folder you want the output to go to.
+	c) If you're doing this on mac or linux or other, let me know if this works for you.
+	
+Instructions:
+1) Run Deck_Extractor.py, I recommend making a shortcut in a convenient place
+2) Enter the name of the deck. You should't type the .ydk extension.
+3) Enter in the desired name of the output file (output is a .txt file).
+
+Added to Github!
